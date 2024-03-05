999dad27329941dcfcffd8d0a578e4df2fc1e13d
afonsocrg <afonso.crg@gmail.com>
2024-03-05 09:26:44 +0000

    remove ; from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Layout from "@pages/Layout";
 import Home from "@pages/Home";
 import { DndProvider } from "react-dnd";
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import './styles.css'
+import { HTML5Backend } from "react-dnd-html5-backend";
+import "./styles.css";
 
 export default function App() {
   return (
     <DndProvider backend={HTML5Backend}>
-      <Layout navBar={<></>} content={<Home />} />;
+      <Layout navBar={<></>} content={<Home />} />
     </DndProvider>
-  )
+  );
 }
